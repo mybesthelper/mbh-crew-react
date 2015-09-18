8e26a2092e661739cf86cc4ac3643a5f8cb072fc
Andy Theriault <artheriault@gmail.com>
2015-09-18 14:01:36 -0700

    task list

--- Styles.js
+++ Styles.js
@@ -12,19 +12,55 @@ var {
 var styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#FFF',
+  },
+  rightContainer: {
+    flex: 1,
+    backgroundColor: '#FFF',
+    marginBottom: 2,
+  },
+  leftContainer: {
+    marginLeft: 5,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+  listView: {
+    paddingTop: 20,
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  header: {
+    height: 50,
+    width: 100,
   },
-  instructions: {
+  button: {
+    backgroundColor: '#3666C6',
+    height: 50,
+    width: 50,
+    shadowRadius: 2,
+    shadowOffset: {width:0, height:2},
+    shadowOpacity: 0.7,
+    borderRadius: 50,
+    shadowColor: 'black',
+  },
+  buttonText: {
+    transform: [{translateY: -12}],
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    lineHeight: 90,
+    color: '#FFFFFF',
+    fontSize: 50,
   },
 });
 
--- index.android.js
+++ index.android.js
@@ -7,31 +7,93 @@
 var React = require('react-native');
 
 var Styles = require('./Styles');
+var Button = require('./Button');
+var TaskList = require('./TaskList');
+
+var data = {
+  tasks:
+    [ { 'id': 1,
+        'type': 'undefined',
+        'title': 'Beeeeeer',
+        'details': 'Happy Hours!',
+        'time': '2015-02-13T17:00:00.000Z',
+        'urgency': 0,
+        'location': 'Vancouver',
+        'latitude': '',
+        'longitude': '',
+        'source': 'source_mycrew_self',
+        'source_mycrew_self': [Object],
+        'source_mycrew_others': {} },
+      { 'id': 2,
+        'type': 'undefined',
+        'title': 'Babysitter needed',
+        'details': 'Party',
+        'time': '2015-03-21T20:00:00.000Z',
+        'urgency': 0,
+        'location': 'V6j1g1',
+        'latitude': 49.2704,
+        'longitude': -123.142,
+        'source': 'source_mycrew_others',
+        'source_mycrew_self': [Object],
+        'source_mycrew_others': [Object] }
+    ]
+}
 
 var {
   AppRegistry,
+  Navigator,
   Text,
   View,
+  Image,
+  ListView,
 } = React;
 
 var MyCrew = React.createClass({
+  getInitialState: function() {
+    return {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  },
+
+  componentDidMount: function() {
+    this.fetchData();
+  },
+
+  fetchData: function() {
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows(data.tasks),
+      loaded: true,
+    });
+  },
+
   render: function() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <TaskList
+        tasks={this.state.dataSource}
+      />
+    );
+  },
+
+  renderLoadingView: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
+        <Image
+          source={require('image!logo')}
+          style={styles.thumbnail}
+        />
       </View>
     );
-  }
+  },
 });
 
 var styles = Styles;
 
 AppRegistry.registerComponent('MyCrew', () => MyCrew);
+
